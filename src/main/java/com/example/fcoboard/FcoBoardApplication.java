086c4f2fe34203408a750364c0d108e40dd91d51
projectmiluju <dnjsdyd0712@naver.com>
2023-12-18 18:49:18 +0900

    feat(초기 설정): 개인 프로젝트 시작

---
+++ src/main/java/com/example/fcoboard/FcoBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.fcoboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcoBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FcoBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fcoboard/FcoBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fcoboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcoBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
